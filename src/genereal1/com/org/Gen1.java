64d8ba0b45f0f8521e6ce9ce852443f141e9cfb5
Ravi <672716@cognizant.com>
2020-04-18 04:07:20 -0600

    Comments

---
+++ src/genereal1/com/org/Gen1.java
@@ -0,0 +1,11 @@
+package genereal1.com.org;
+
+public class Gen1 {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\672716\\eclipse-workspace\\genereal1\\drivers\\chromedriver.exe");
+	
+	System.out.println("first");
+	
+	System.out.println("second");
+}
+}
