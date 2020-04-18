0e2759485daf98aaad942118f098dae2383660fa
Ravi <672716@cognizant.com>
2020-04-18 04:37:30 -0600

    new

--- src/genereal1/com/org/Gen1.java
+++ src/genereal1/com/org/Gen1.java
@@ -5,7 +5,9 @@ public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\672716\\eclipse-workspace\\genereal1\\drivers\\chromedriver.exe");
 	
 	System.out.println("first");
-	
+	System.out.println("added");
+	System.out.println("add 2");
+	System.out.println("add3");
 	System.out.println("second");
 }
 }
